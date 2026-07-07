e2f9322dc28ca623e97f9ec4b6990962a040a21a
Sam <1ga25cs204@gat.ac.in>
2026-07-07 12:13:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function showMessage(){
+
+    alert(
+        "Welcome to my GitHub Portfolio"
+    );
+
+}
+
+
+const skills = [
+    "AI",
+    "Machine Learning",
+    "Full Stack",
+    "Cloud"
+];
+
+
+skills.forEach(skill=>{
+    console.log(skill);
+});
